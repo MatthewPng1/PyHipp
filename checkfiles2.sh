9027af096a948ecaa1ec3fc86b41d417b006f26c
AWS ParallelCluster user <ec2-user@ip-10-0-13-1.ap-southeast-1.compute.internal>
2024-09-06 13:37:37 +0000

    new file:   checkfiles.sh 	new file:   checkfiles2.sh 	new file:   pipe2.sh 	new file:   pipe2a.sh 	new file:   pipeline-slurm.sh 	new file:   removefiles.sh 	new file:   rplhighpass-sort-slurm.sh 	new file:   rpllfp-slurm.sh 	new file:   rplparallel-slurm.sh 	new file:   rplsplit-slurm.sh 	new file:   rs1-slurm.sh 	new file:   rs1a-slurm.sh 	new file:   rs2-slurm.sh 	new file:   rs2a-slurm.sh 	new file:   rs3-slurm.sh 	new file:   rs3a-slurm.sh 	new file:   rs4-slurm.sh 	new file:   rs4a-slurm.sh 	new file:   rse-slurm.sh

---
+++ checkfiles2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "Number of hkl files"
+find . -name "*.hkl" | grep -v -e spiketrain -e mountains | wc -l
+
+echo "Number of mda files"
+find mountains -name "firings.mda" | wc -l
+
+echo "#==========================================================="
+echo "Start Times"
+for file in *-slurm.queue1-*.out; do
+    if [[ -f "$file" ]]; then
+        start_time=$(grep -oP 'time.struct_time\(.*\)' "$file" | head -n 1)
+        echo "==> $file <=="
+        echo "$start_time"
+    fi
+done
+
+echo "End Times"
+for file in *-slurm.queue1-*.out; do
+    if [[ -f "$file" ]]; then
+	start_time=$(grep -oP 'time.struct_time\(.*\)' "$file" | head -n 1)
+        end_time=$(grep -oP 'time.struct_time\(.*\)' "$file" | tail -n 1)
+	echo "==> $file <=="
+	echo "$end_time"
+	read -r start_hour start_minute start_second <<< $(echo "$start_time" | grep -oP 'tm_hour=\K[0-9]+|tm_min=\K[0-9]+|tm_sec=\K[0-9]+')
+        #echo "Start time extracted: Hour=$start_hour Minute=$start_minute Second=$start_second"  # Debugging
+	#echo "$start_seconds"
+	read -r end_hour end_minute end_second <<< $(echo "$end_time" | grep -oP 'tm_hour=\K[0-9]+|tm_min=\K[0-9]+|tm_sec=\K[0-9]+')
+        #echo "End time extracted: Hour=$end_hour Minute=$end_minute Second=$end_second"  # Debugging
+	#echo "$end_seconds"
+	start_seconds=$(( start_hour * 3600 + start_minute * 60 + start_second ))
+        end_seconds=$(( end_hour * 3600 + end_minute * 60 + end_second ))
+	duration=$((end_seconds - start_seconds))
+	echo "$duration"
+	aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:586794440329:awsnotify --message "JobDone"
+    fi
+done
+
+
+echo "#==========================================================="
